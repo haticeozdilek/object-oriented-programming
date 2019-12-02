f40522cd62647bcece8e0b05daacac90669a6aa7
Hatice Özdilek <48315417+haticeozdilek@users.noreply.github.com>
2019-12-02 16:16:26 +0300

    Add files via upload

---
+++ TauSayisi.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class TauSayisi {
+
+	public static void main(String[] args) {
+		int sayac=0,i=1;
+		int sayi;
+		
+		Scanner sayiGir=new Scanner(System.in);
+		System.out.println("Lütfen istediginiz sayiyi giriniz :");
+		sayi=sayiGir.nextInt();
+		
+		while(i!=sayi+1) {
+			if(sayi%i == 0) {
+				sayac++;
+			
+			}
+			i++;
+		}
+		
+		if(sayi % sayac==0) 
+			System.out.println("Girilen sayi Tau Sayisidir");
+		else
+			System.out.println("Tau Sayisi degildir");
+		
+	}
+
+}
